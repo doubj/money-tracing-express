ba4d5921d84f15a38566139dc28346353e39ce23
doubj <935208113@qq.com>
2021-04-25 00:16:21 +0800

    配置修改

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const InitManager = require("./core/init");
 InitManager.initCore(app);
 
-const port = 3005;
+const port = process.env.PORT || 3005;
 app.listen(port, () => {
   console.log(`express is listening at http://localhost:${port}`);
 });
